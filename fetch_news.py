ec2f0a06a8d97bedfb3310f25919fa2015ca3e3c
bene-s-dev <282218590+bene-s-dev@users.noreply.github.com>
2026-08-07 21:49:51 +0200

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -4,7 +4,7 @@ from supabase import create_client
 
 SUPABASE_URL = "https://jyoxxkngxxfmiskfxndp.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5b3h4a25neHhmbWlza2Z4bmRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5Mjg4NTQsImV4cCI6MjEwMTUwNDg1NH0.g6iDSYtD9rCU8SMKdpqg8OTIK8VYueYbbXvQe2ouwXg"
-supabase = create_client(, SUPABASE_KEY)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 query = 'Wero OR "Martina Weimert" OR "EPI Company"'
 rss_url = f'https://news.google.com/rss/search?q={query}&hl=de&gl=DE&ceid=DE:de'
